315543880dc057e31cb8672374259bbde05c3520
eddietrrs28 <eddietrrs28@gmail.com>
2021-03-17 18:54:59 -0600

    Tipos de datos basicos y variables

--- Practica_1/JS/index.js
+++ Practica_1/JS/index.js
@@ -1,6 +1,23 @@
 let paragraph = document.getElementById('mainParagraph');
-paragraph.addEventListener('click',WriteParagraph);
+let btnNumeric = document.getElementById('numeric')
+let btnText = document.getElementById('text')
+let btnBool = document.getElementById('bool')
 
-function WriteParagraph(){
-    paragraph.textContent = 'Hola Mundo'
+btnNumeric.addEventListener('click',WriteNumericValue);
+btnText.addEventListener('click',WriteTextValue);
+btnBool.addEventListener('click',WriteBoolValue);
+
+function WriteNumericValue(){
+    let numericValue = 2+2;
+    paragraph.textContent = numericValue;
+}
+
+function WriteTextValue(){
+    let textValue = "Hola Mundo";
+    paragraph.textContent = textValue;
+}
+
+function WriteBoolValue(){
+    let boolValue = true;
+    paragraph.textContent = boolValue;
 }
