c60238b7a2356acb12d01a40a1f621b7951419c1
eddietrrs28 <eddietrrs28@gmail.com>
2021-04-04 15:51:26 -0500

    Se termina practica de JS

---
+++ Practica_2(SumaDosNumeros)/JS/index.js
@@ -0,0 +1,11 @@
+
+let btnSumar = document.getElementById('btnSuma');
+
+btnSumar.addEventListener('click', sumarDosNumeros);
+
+function sumarDosNumeros(){
+    let numero1 = document.getElementById('numero1').value;
+    let numero2 = document.getElementById('numero2').value;
+    console.log(numero1);
+    console.log(numero2);
+}
