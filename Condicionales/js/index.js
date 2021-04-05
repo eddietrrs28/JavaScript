92e28ccad7e134d5b43bc1877d3885945f91c337
eddietrrs28 <eddietrrs28@gmail.com>
2021-04-05 17:57:38 -0500

    Se termina ejemplo

---
+++ Condicionales/js/index.js
@@ -0,0 +1,18 @@
+const btnEvNum = document.getElementById("btnEvNum").value;
+
+btnEvNum.addEventListener('click', evaluarNumeros)
+
+function evaluarNumeros(){
+    const inpNumero1 = Number(document.getElementById('inpNumero1').value);
+    const inpNumero2 = Number(document.getElementById('inpNumero1').value);
+
+    let numeroMayor = 0;
+    const alertaResultado = document.getElementById("resultado");
+
+    if(inpNumero1 > inpNumero2){
+        alertaResultado.innerHTML = 'El numero '+inpNumero1+' es mayor';
+    }
+    else{
+        alertaResultado.innerHTML = 'El numero '+inpNumero2+' es menor';
+    }
+}
